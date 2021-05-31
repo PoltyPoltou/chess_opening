1b0f5d14c20f30906cb76b8888a0d74c4b4b44d2
NP-Polty <polt.gun@gmail.com>
2021-05-31 02:22:15 +0100

    mini test in main with uci moves

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,20 @@
 #include <qt5/QtWidgets/QApplication>
 int main(int argc, char *argv[])
 {
+    chess::BoardState b;
+    std::string buffer = "";
+    while (buffer != "a1a1")
+    {
+        std::cout << b << std::endl;
+        std::cin >> buffer;
+        chess::Move mv(buffer);
+        if (b.isLegal(mv))
+        {
+            b.playMove(mv);
+        }
+    }
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    chess::BoardState b;
-    std::cout << b << std::endl;
     return a.exec();
 }
